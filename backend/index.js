5a48c3e3501275ba0432be9c877cdcbf8c908771
Inferno <vikasanthwal985@gmail.com>
2026-07-02 23:02:53 +0530

    production ready

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { config } from "dotenv";
+import cloudinary from "cloudinary";
+import dbConnection from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import path from "path";
+
+config({ path: "./.env" });
+
+const app = express();
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+  api_key: process.env.CLOUDINARY_CLIENT_API,
+  api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+});
+
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at port ${process.env.PORT}`);
+});
